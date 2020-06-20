b10990f256bd16c9aaf43969109201bf9da3c5ef
ssorry123 <syd03160@gmail.com>
2020-06-20 21:55:20 +0900

    ch5

--- ch99/blog/views.py
+++ ch99/blog/views.py
@@ -23,25 +23,31 @@ class PostLV(ListView) :
 # DetailView
 class PostDV(DetailView):
 	model = Post
+	template_name = 'blog/post_detail.html'
 
 # ArchiveView
 class PostAV(ArchiveIndexView):
 	model = Post
 	date_field = 'modify_dt'
+	template_name = 'blog/post_archive.html'
 	
 class PostYAV(YearArchiveView):
 	model = Post
 	date_field = 'modify_dt'
 	make_object_list = True
+	template_name = 'blog/post_archive_year.html'
 
 class PostMAV(MonthArchiveView):
 	model = Post
 	date_field = 'modify_dt'
+	template_name = 'blog/post_archive_month.html'
 
 class PostDAV(DayArchiveView):
 	model = Post
 	date_field = 'modify_dt'
+	template_name = 'blog/post_archive_day.html'
 	
 class PostTAV(TodayArchiveView):
 	model = Post
 	date_field = 'modify_dt'
+	template_name = 'blog/post_archive_year.html'
